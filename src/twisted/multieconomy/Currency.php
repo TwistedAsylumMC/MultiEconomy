44ac8134def88a8041c45cdc5e66c878fbd6f917
Adam Matthew <larryPeter132@gmail.com>
2020-07-26 17:46:03 +0800

    Fix code formatting.

--- src/twisted/multieconomy/Currency.php
+++ src/twisted/multieconomy/Currency.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace twisted\multieconomy;
 
@@ -15,51 +15,51 @@ use function min;
 use function number_format;
 use function strtolower;
 
-class Currency{
+class Currency {
 
-    /** @var string $name */
-    private $name;
+	/** @var string $name */
+	private $name;
 
-    /** @var string $symbol */
-    private $symbol;
-    /** @var bool $symbolAfter */
-    private $symbolAfter;
+	/** @var string $symbol */
+	private $symbol;
+	/** @var bool $symbolAfter */
+	private $symbolAfter;
 
-    /** @var float $startingAmount */
-    private $startingAmount;
+	/** @var float $startingAmount */
+	private $startingAmount;
 
-    /** @var float $minAmount */
-    private $minAmount;
-    /** @var float $maxAmount */
-    private $maxAmount;
+	/** @var float $minAmount */
+	private $minAmount;
+	/** @var float $maxAmount */
+	private $maxAmount;
 
-    /** @var Database */
-    private $database;
+	/** @var Database */
+	private $database;
 	/** @var float[] */
-    private $cache = [];
+	private $cache = [];
 
-    public function __construct(string $name, string $symbol, bool $symbolAfter, float $startingAmount, float $minAmount, float $maxAmount){
-        $this->name = $name;
-        $this->symbol = $symbol;
-        $this->symbolAfter = $symbolAfter;
-        $this->startingAmount = max(0.0, $startingAmount);
-        $this->minAmount = min($minAmount, $maxAmount);
-        $this->maxAmount = max($minAmount, $maxAmount);
+	public function __construct(string $name, string $symbol, bool $symbolAfter, float $startingAmount, float $minAmount, float $maxAmount){
+		$this->name = $name;
+		$this->symbol = $symbol;
+		$this->symbolAfter = $symbolAfter;
+		$this->startingAmount = max(0.0, $startingAmount);
+		$this->minAmount = min($minAmount, $maxAmount);
+		$this->maxAmount = max($minAmount, $maxAmount);
 
-        $this->loadDatabase();
-    }
+		$this->loadDatabase();
+	}
 
-    /**
-     * Internal function to load & prepare the database
-     * Loads then caches any data from existing databases
-     */
-    private function loadDatabase() : void{
-        $path = MultiEconomy::getInstance()->getDataFolder() . "currencies/";
+	/**
+	 * Internal function to load & prepare the database
+	 * Loads then caches any data from existing databases
+	 */
+	private function loadDatabase(): void{
+		$path = MultiEconomy::getInstance()->getDataFolder() . "currencies/";
 
-        $config = MultiEconomy::getInstance()->getConfig();
-        $info = $config->get("database");
+		$config = MultiEconomy::getInstance()->getConfig();
+		$info = $config->get("database");
 
-        switch(($provider = strtolower($info["provider"])) ?? "sqlite"){
+		switch(($provider = strtolower($info["provider"])) ?? "sqlite"){
 			case "yml":
 			case "json":
 				$this->database = new NormalDatabase($path . $this->getLowerName() . "." . $provider);
@@ -83,233 +83,233 @@ class Currency{
 		});
 	}
 
-    /**
-     * Gets the name of the currency
-     * in all lowercase letters
-     *
-     * @return string
-     */
-    public function getLowerName() : string{
-        return strtolower($this->name);
-    }
+	/**
+	 * Gets the name of the currency
+	 * in all lowercase letters
+	 *
+	 * @return string
+	 */
+	public function getLowerName(): string{
+		return strtolower($this->name);
+	}
 
-    /**
-     * Get the name of the currency in
-     * it's exact casing in the config
-     *
-     * @return string
-     */
-    public function getName() : string{
-        return $this->name;
-    }
+	/**
+	 * Get the name of the currency in
+	 * it's exact casing in the config
+	 *
+	 * @return string
+	 */
+	public function getName(): string{
+		return $this->name;
+	}
 
-    /**
-     * Gets the symbol/sign for the currency
-     *
-     * @return string
-     */
-    public function getSymbol() : string{
-        return $this->symbol;
-    }
+	/**
+	 * Gets the symbol/sign for the currency
+	 *
+	 * @return string
+	 */
+	public function getSymbol(): string{
+		return $this->symbol;
+	}
 
-    /**
-     * Used for formatting balances in the currency
-     * Determines wether the symbol/sign should go
-     * before or after the balance
-     *
-     * @return bool
-     */
-    public function isSymbolAfter() : bool{
-        return $this->symbolAfter;
-    }
+	/**
+	 * Used for formatting balances in the currency
+	 * Determines wether the symbol/sign should go
+	 * before or after the balance
+	 *
+	 * @return bool
+	 */
+	public function isSymbolAfter(): bool{
+		return $this->symbolAfter;
+	}
 
-    /**
-     * Uses isSymbolAfter() to format the balance correctly
-     *
-     * @param float $balance
-     *
-     * @return string
-     */
-    public function formatBalance(float $balance) : string{
-        return $this->symbolAfter ? number_format($balance) . $this->symbol : $this->symbol . number_format($balance);
-    }
+	/**
+	 * Uses isSymbolAfter() to format the balance correctly
+	 *
+	 * @param float $balance
+	 *
+	 * @return string
+	 */
+	public function formatBalance(float $balance): string{
+		return $this->symbolAfter ? number_format($balance) . $this->symbol : $this->symbol . number_format($balance);
+	}
 
-    /**
-     * Gets the default balance for players
-     * who do not have an existing balance
-     *
-     * @return float
-     */
-    public function getStartingAmount() : float{
-        return $this->startingAmount;
-    }
+	/**
+	 * Gets the default balance for players
+	 * who do not have an existing balance
+	 *
+	 * @return float
+	 */
+	public function getStartingAmount(): float{
+		return $this->startingAmount;
+	}
 
-    /**
-     * Gets the minimum amount a balance can have at once
-     *
-     * @return float
-     */
-    public function getMinAmount() : float{
-        return $this->minAmount;
-    }
+	/**
+	 * Gets the minimum amount a balance can have at once
+	 *
+	 * @return float
+	 */
+	public function getMinAmount(): float{
+		return $this->minAmount;
+	}
 
-    /**
-     * Gets the maximum amount a balance can have at once
-     *
-     * @return float
-     */
-    public function getMaxAmount() : float{
-        return $this->maxAmount;
-    }
+	/**
+	 * Gets the maximum amount a balance can have at once
+	 *
+	 * @return float
+	 */
+	public function getMaxAmount(): float{
+		return $this->maxAmount;
+	}
 
 	public function save(){
 		$this->database->shutdown();
 	}
 
-    /**
-     * Adds an amount to the player's balance
-     *
-     * @param string $username
-     * @param float  $amount
-     *
-     * @return bool
-     */
-    public function addToBalance(string $username, float $amount) : bool{
-        if($amount <= 0){
-            return $amount !== 0 && $this->removeFromBalance($username, abs($amount));
-        }
+	/**
+	 * Adds an amount to the player's balance
+	 *
+	 * @param string $username
+	 * @param float $amount
+	 *
+	 * @return bool
+	 */
+	public function addToBalance(string $username, float $amount): bool{
+		if($amount <= 0){
+			return $amount !== 0 && $this->removeFromBalance($username, abs($amount));
+		}
 
-        if(isset($this->cache[strtolower($username)])){
-            $this->cache[strtolower($username)] += $amount;
-        }else{
-            $this->cache[strtolower($username)] = $amount;
-        }
+		if(isset($this->cache[strtolower($username)])){
+			$this->cache[strtolower($username)] += $amount;
+		}else{
+			$this->cache[strtolower($username)] = $amount;
+		}
 
-        $this->validateBalance($username);
+		$this->validateBalance($username);
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Removes an amount to the player's balance
-     *
-     * @param string $username
-     * @param float  $amount
-     *
-     * @return bool
-     */
-    public function removeFromBalance(string $username, float $amount) : bool{
-        if($amount <= 0){
-            return $amount !== 0 && $this->addToBalance($username, abs($amount));
-        }
+	/**
+	 * Removes an amount to the player's balance
+	 *
+	 * @param string $username
+	 * @param float $amount
+	 *
+	 * @return bool
+	 */
+	public function removeFromBalance(string $username, float $amount): bool{
+		if($amount <= 0){
+			return $amount !== 0 && $this->addToBalance($username, abs($amount));
+		}
 
-        if(isset($this->cache[strtolower($username)])){
-            $this->cache[strtolower($username)] -= $amount;
-        }else{
-            $this->cache[strtolower($username)] = $amount;
-        }
+		if(isset($this->cache[strtolower($username)])){
+			$this->cache[strtolower($username)] -= $amount;
+		}else{
+			$this->cache[strtolower($username)] = $amount;
+		}
 
-        $this->validateBalance($username);
+		$this->validateBalance($username);
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Checks if the player's balance is within the min & max values
-     * It then automatically corrects the player's balance if needed
-     *
-     * @param string $username
-     *
-     * @return bool
-     */
-    public function validateBalance(string $username) : bool{
-    	$valid = true;
-        if(!isset($this->cache[strtolower($username)])){
-            if($this->startingAmount > 0){
-                $this->cache[strtolower($username)] = $this->startingAmount;
-            }
-
-			$valid = false;
-        }
-
-        if($this->cache[strtolower($username)] < $this->minAmount){
-            $this->cache[strtolower($username)] = $this->minAmount;
+	/**
+	 * Checks if the player's balance is within the min & max values
+	 * It then automatically corrects the player's balance if needed
+	 *
+	 * @param string $username
+	 *
+	 * @return bool
+	 */
+	public function validateBalance(string $username): bool{
+		$valid = true;
+		if(!isset($this->cache[strtolower($username)])){
+			if($this->startingAmount > 0){
+				$this->cache[strtolower($username)] = $this->startingAmount;
+			}
 
 			$valid = false;
-        }
+		}
 
-        if($this->cache[strtolower($username)] > $this->maxAmount){
-            $this->cache[strtolower($username)] = $this->maxAmount;
+		if($this->cache[strtolower($username)] < $this->minAmount){
+			$this->cache[strtolower($username)] = $this->minAmount;
 
 			$valid = false;
-        }
+		}
+
+		if($this->cache[strtolower($username)] > $this->maxAmount){
+			$this->cache[strtolower($username)] = $this->maxAmount;
+
+			$valid = false;
+		}
 		if(!$valid) $this->database->setBalance($username, $this->cache[strtolower($username)]);
 
-        return $valid;
-    }
+		return $valid;
+	}
 
-    /**
-     * Get a specific player's balance
-     * Returns the starting amount if they do not
-     * have a balance, and adds them to the cache
-     *
-     * @param string $username
-     *
-     * @return float
-     */
-    public function getBalance(string $username) : float{
-        if(!isset($this->cache[strtolower($username)])){
-            $this->cache[strtolower($username)] = $this->startingAmount;
+	/**
+	 * Get a specific player's balance
+	 * Returns the starting amount if they do not
+	 * have a balance, and adds them to the cache
+	 *
+	 * @param string $username
+	 *
+	 * @return float
+	 */
+	public function getBalance(string $username): float{
+		if(!isset($this->cache[strtolower($username)])){
+			$this->cache[strtolower($username)] = $this->startingAmount;
 
-            return $this->startingAmount;
-        }
+			return $this->startingAmount;
+		}
 
-        return $this->cache[strtolower($username)];
-    }
+		return $this->cache[strtolower($username)];
+	}
 
-    /**
-     * Set a player's balance to the given value
-     *
-     * @param string $username
-     * @param float  $amount
-     *
-     * @return bool
-     */
-    public function setBalance(string $username, float $amount) : bool{
-        $this->cache[strtolower($username)] = $amount;
+	/**
+	 * Set a player's balance to the given value
+	 *
+	 * @param string $username
+	 * @param float $amount
+	 *
+	 * @return bool
+	 */
+	public function setBalance(string $username, float $amount): bool{
+		$this->cache[strtolower($username)] = $amount;
 
-        $this->validateBalance($username);
+		$this->validateBalance($username);
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Returns the top $size balances for the currency,
-     * an ordered float array from richest to poorest
-     * in the format of (string)username => (float)balance
-     *
-     * @param int $size
-     * @param int $page
-     *
-     * @return float[]
-     */
-    public function getTopBalances(int $size, int $page) : array{
-        $balances = $this->getAllBalances();
-        asort($balances);
-        $balances = array_chunk($balances, $size, true);
+	/**
+	 * Returns the top $size balances for the currency,
+	 * an ordered float array from richest to poorest
+	 * in the format of (string)username => (float)balance
+	 *
+	 * @param int $size
+	 * @param int $page
+	 *
+	 * @return float[]
+	 */
+	public function getTopBalances(int $size, int $page): array{
+		$balances = $this->getAllBalances();
+		asort($balances);
+		$balances = array_chunk($balances, $size, true);
 
-        $page = min(count($balances), max(1, $page));
+		$page = min(count($balances), max(1, $page));
 
-        return $balances[$page - 1] ?? [];
-    }
+		return $balances[$page - 1] ?? [];
+	}
 
-    /**
-     * Returns all the balances in float array with
-     * the format (string)username => (float)balance
-     *
-     * @return float[]
-     */
-    public function getAllBalances() : array{
-        return $this->cache;
-    }
+	/**
+	 * Returns all the balances in float array with
+	 * the format (string)username => (float)balance
+	 *
+	 * @return float[]
+	 */
+	public function getAllBalances(): array{
+		return $this->cache;
+	}
 }
--- src/twisted/multieconomy/MultiEconomy.php
+++ src/twisted/multieconomy/MultiEconomy.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace twisted\multieconomy;
 
@@ -23,19 +23,24 @@ use function is_array;
 use function str_replace;
 use function strtolower;
 
-class MultiEconomy extends PluginBase implements Listener{
+class MultiEconomy extends PluginBase implements Listener {
 
-    /** @var MultiEconomy */
-    private static $instance;
+	public static $replaceDialect = null;
+	/** @var MultiEconomy */
+	private static $instance;
+	/** @var Currency[] */
+	private $currencies = [];
 
-    /** @var Currency[] */
-    private $currencies = [];
+	/**
+	 * @return MultiEconomy
+	 */
+	public static function getInstance(): MultiEconomy{
+		return self::$instance;
+	}
 
-    public function onLoad(){
-        self::$instance = $this;
-    }
-
-    public static $replaceDialect = null;
+	public function onLoad(){
+		self::$instance = $this;
+	}
 
 	public function getResource(string $filename){
 		// A little bit hack for mysql statements
@@ -63,7 +68,7 @@ class MultiEconomy extends PluginBase implements Listener{
 		return parent::getResource($filename);
 	}
 
-    public function onEnable() : void{
+	public function onEnable(): void{
 		@mkdir($this->getDataFolder() . "currencies");
 		@mkdir($this->getDataFolder() . "lang");
 		@mkdir($this->getDataFolder() . "temp");
@@ -78,144 +83,137 @@ class MultiEconomy extends PluginBase implements Listener{
 		}
 		$this->saveResource("lang/" . $this->getLanguageCode() . ".yml");
 
-        $currencies = $config->get("currencies", []);
-        if(is_array($currencies)){
-            foreach($currencies as $currency => $data){
-                $name = (string) ($data["name"] ?? $currency);
-                $symbol = (string) ($data["symbol"] ?? "$");
-                $symbolAfter = (bool) ($data["symbol-after"] ?? false);
-                $starting = (int) ($data["starting-amount"] ?? 0);
-                $min = (int) ($data["min-amount"] ?? 0);
-                $max = (int) ($data["max-amount"] ?? 0);
+		$currencies = $config->get("currencies", []);
+		if(is_array($currencies)){
+			foreach($currencies as $currency => $data){
+				$name = (string)($data["name"] ?? $currency);
+				$symbol = (string)($data["symbol"] ?? "$");
+				$symbolAfter = (bool)($data["symbol-after"] ?? false);
+				$starting = (int)($data["starting-amount"] ?? 0);
+				$min = (int)($data["min-amount"] ?? 0);
+				$max = (int)($data["max-amount"] ?? 0);
 
-                $this->registerCurrency(new Currency($name, $symbol, $symbolAfter, $starting, $min, $max));
-            }
-        }
+				$this->registerCurrency(new Currency($name, $symbol, $symbolAfter, $starting, $min, $max));
+			}
+		}
 
-        $this->getServer()->getCommandMap()->registerAll("MultiCommand", [
-            new AddToBalanceCommand($this),
-            new BalanceCommand($this),
-            new BalanceTopCommand($this),
-            new PayCommand($this),
-            new RemoveFromBalanceCommand($this),
-            new SetBalanceCommand($this)
-        ]);
+		$this->getServer()->getCommandMap()->registerAll("MultiCommand", [
+			new AddToBalanceCommand($this),
+			new BalanceCommand($this),
+			new BalanceTopCommand($this),
+			new PayCommand($this),
+			new RemoveFromBalanceCommand($this),
+			new SetBalanceCommand($this),
+		]);
 
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
 
-    /**
-     * Gets the 3 letter language code used for the plugin
-     *
-     * @return string
-     */
-    public function getLanguageCode() : string{
-        $lang = (string) $this->getConfig()->get("lang");
-        $supported = [
-            "eng",
-            "chn"
-        ];
+	/**
+	 * Gets the 3 letter language code used for the plugin
+	 *
+	 * @return string
+	 */
+	public function getLanguageCode(): string{
+		$lang = (string)$this->getConfig()->get("lang");
+		$supported = [
+			"eng",
+			"chn",
+		];
 
-        return in_array(strtolower($lang), $supported, true) ? strtolower($lang) : "eng";
-    }
+		return in_array(strtolower($lang), $supported, true) ? strtolower($lang) : "eng";
+	}
 
-    /**
-     * Registers a new currency, indexed by its lowercase name
-     *
-     * @param Currency $currency
-     *
-     * @return bool
-     */
-    public function registerCurrency(Currency $currency) : bool{
-        $this->currencies[$currency->getLowerName()] = $currency;
+	/**
+	 * Registers a new currency, indexed by its lowercase name
+	 *
+	 * @param Currency $currency
+	 *
+	 * @return bool
+	 */
+	public function registerCurrency(Currency $currency): bool{
+		$this->currencies[$currency->getLowerName()] = $currency;
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Returns all the registered currencies
-     *
-     * @return Currency[]
-     */
-    public function getCurrencies() : array{
-        return $this->currencies;
-    }
+	/**
+	 * Get a currency by its name, returns null if not found
+	 *
+	 * @param string $name
+	 *
+	 * @return Currency|null
+	 */
+	public function getCurrency(string $name): ?Currency{
+		return $this->currencies[strtolower($name)] ?? null;
+	}
 
-    /**
-     * @return MultiEconomy
-     */
-    public static function getInstance() : MultiEconomy{
-        return self::$instance;
-    }
+	public function onDisable(): void{
+		foreach($this->getCurrencies() as $currency){
+			$currency->save();
+		}
+	}
 
-    /**
-     * Get a currency by its name, returns null if not found
-     *
-     * @param string $name
-     *
-     * @return Currency|null
-     */
-    public function getCurrency(string $name) : ?Currency{
-        return $this->currencies[strtolower($name)] ?? null;
-    }
+	/**
+	 * Returns all the registered currencies
+	 *
+	 * @return Currency[]
+	 */
+	public function getCurrencies(): array{
+		return $this->currencies;
+	}
 
-    public function onDisable() : void{
-        foreach($this->getCurrencies() as $currency){
-            $currency->save();
-        }
-    }
+	/**
+	 * Gets the translation from the language
+	 * config and parses all parameters
+	 *
+	 * @param string $identifier
+	 * @param array $params
+	 *
+	 * @return string
+	 */
+	public function translateMessage(string $identifier, array $params = []): string{
+		$lang = $this->getLanguageConfig()->getAll();
 
-    /**
-     * Gets the translation from the language
-     * config and parses all parameters
-     *
-     * @param string $identifier
-     * @param array  $params
-     *
-     * @return string
-     */
-    public function translateMessage(string $identifier, array $params = []) : string{
-        $lang = $this->getLanguageConfig()->getAll();
+		$message = (string)($lang[$identifier] ?? "&8[&9MultiEconomy&8]&7 Language identifier not found");
+		$prefix = (string)($lang["prefix"] ?? "&8[&9MultiEconomy&8]&7");
 
-        $message = (string) ($lang[$identifier] ?? "&8[&9MultiEconomy&8]&7 Language identifier not found");
-        $prefix = (string) ($lang["prefix"] ?? "&8[&9MultiEconomy&8]&7");
+		$search = array_merge(["{prefix}", "&"], array_map(static function(string $key): string{
+			return "{" . $key . "}";
+		}, array_keys($params)));
+		$replace = array_merge([$prefix, TextFormat::ESCAPE], array_values($params));
 
-        $search = array_merge(["{prefix}", "&"], array_map(static function(string $key) : string{
-            return "{" . $key . "}";
-        }, array_keys($params)));
-        $replace = array_merge([$prefix, TextFormat::ESCAPE], array_values($params));
+		$message = str_replace($search, $replace, $message);
 
-        $message = str_replace($search, $replace, $message);
+		return $message;
+	}
 
-        return $message;
-    }
+	/**
+	 * Returns the configuration file for the plugin's language
+	 *
+	 * @return Config
+	 */
+	public function getLanguageConfig(): Config{
+		return new Config($this->getDataFolder() . "lang/" . $this->getLanguageCode() . ".yml", Config::YAML);
+	}
 
-    /**
-     * Returns the configuration file for the plugin's language
-     *
-     * @return Config
-     */
-    public function getLanguageConfig() : Config{
-        return new Config($this->getDataFolder() . "lang/" . $this->getLanguageCode() . ".yml", Config::YAML);
-    }
+	/**
+	 * Returns a string array of all the currency names
+	 *
+	 * @return string[]
+	 */
+	public function getCurrencyNames(): array{
+		return array_map(static function(Currency $currency): string{
+			return $currency->getName();
+		}, $this->currencies);
+	}
 
-    /**
-     * Returns a string array of all the currency names
-     *
-     * @return string[]
-     */
-    public function getCurrencyNames() : array{
-        return array_map(static function(Currency $currency) : string{
-            return $currency->getName();
-        }, $this->currencies);
-    }
-
-    /**
-     * @param PlayerJoinEvent $event
-     */
-    public function onPlayerJoin(PlayerJoinEvent $event) : void{
-        foreach($this->getCurrencies() as $currency){
-            $currency->validateBalance($event->getPlayer()->getName());
-        }
-    }
+	/**
+	 * @param PlayerJoinEvent $event
+	 */
+	public function onPlayerJoin(PlayerJoinEvent $event): void{
+		foreach($this->getCurrencies() as $currency){
+			$currency->validateBalance($event->getPlayer()->getName());
+		}
+	}
 }
